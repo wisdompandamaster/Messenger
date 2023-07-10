92b2b399e495e9ba105e7f76a200a61484ff9cb9
wisdompandamaster <2532569059@qq.com>
2023-07-10 13:00:39 +0800

    feat(Messages):complete the structure of message

---
+++ app/api/messages/route.ts
@@ -0,0 +1,70 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/app/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { message, image, conversationId } = body;
+
+    // 如果没有用户id或邮箱
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        body: message,
+        image: image,
+        conversation: {
+          connect: {
+            id: conversationId,
+          },
+        },
+        sender: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+        seen: {
+          connect: {
+            id: currentUser.id,
+          },
+        },
+      },
+      include: {
+        seen: true,
+        sender: true,
+      },
+    });
+
+    // 后面用来实时更新conversation
+    const updatedConversation = await prisma.conversation.update({
+      where: {
+        id: conversationId,
+      },
+      data: {
+        lastMessageAt: new Date(),
+        messages: {
+          connect: {
+            id: newMessage.id,
+          },
+        },
+      },
+      include: {
+        users: true,
+        messages: {
+          include: {
+            seen: true,
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error: any) {
+    console.log(error, "ERROR_MESSAGES");
+    return new NextResponse("InternalError", { status: 500 });
+  }
+}
