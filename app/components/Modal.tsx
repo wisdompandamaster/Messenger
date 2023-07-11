61379bfc7ae82796af4bbf294b748df1cd48a77a
wisdompandamaster <2532569059@qq.com>
2023-07-12 03:51:55 +0800

    fix(Modal):change the opactiy to opacity

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -25,25 +25,24 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
         >
           <div
             className='
-              fixed 
-              inset-0 
-              bg-gray-500 
-              bg-opacity-75 
-              transition-opacity
-            '
+                  fixed
+                  inset-0
+                  bg-gray-500
+                  bg-opacity-75
+                  transition-opacity
+                '
           />
         </Transition.Child>
-
         <div className='fixed inset-0 z-10 overflow-y-auto'>
           <div
             className='
-              flex 
-              min-h-full 
-              items-center 
-              justify-center 
-              p-4 
-              text-center 
-              sm:p-0
+             flex
+             min-h-full
+             items-center
+             text-center
+             justify-center
+             sm:p-0
+             p-4
             '
           >
             <Transition.Child
@@ -53,36 +52,35 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
               enterTo='opacity-100 translate-y-0 sm:scale-100'
               leave='ease-in duration-200'
               leaveFrom='opacity-100 translate-y-0 sm:scale-100'
-              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              leaveTo='opactiy-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
               <Dialog.Panel
                 className='
-                  relative 
-                  transform 
-                  overflow-hidden 
-                  rounded-lg 
-                  bg-white 
-                  px-4 
+                  relative
+                  transform
+                  overflow-hidden
+                  rounded-lg
+                  bg-white
+                  px-4
                   pb-4
-                  pt-5 
-                  text-left 
-                  shadow-xsl 
+                  text-left
+                  shadow-xsl
                   transition-all
                   w-full
-                  sm:my-8 
-                  sm:w-full 
-                  sm:max-w-lg 
+                  sm:my-8
+                  sm:w-full
+                  sm:max-w-lg
                   sm:p-6
                 '
               >
                 <div
                   className='
-                    absolute 
-                    right-0 
-                    top-0 
-                    hidden 
-                    pr-4 
-                    pt-4 
+                    absolute
+                    right-0
+                    top-0
+                    hidden
+                    pr-4
+                    pt-4
                     sm:block
                     z-10
                   '
@@ -90,19 +88,24 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
                   <button
                     type='button'
                     className='
-                      rounded-md 
-                      bg-white 
-                      text-gray-400 
-                      hover:text-gray-500 
-                      focus:outline-none 
-                      focus:ring-2 
-                      focus:ring-sky-500 
-                      focus:ring-offset-2
-                    '
+                          rounded-md
+                          bg-white
+                          text-gray-400
+                          hover:text-gray-500
+                          focus:outline-none
+                          focus:ring-2
+                          focus:ring-sky-500
+                          focus:ring-offset-2
+                        '
                     onClick={onClose}
                   >
                     <span className='sr-only'>Close</span>
-                    <IoClose className='h-6 w-6' aria-hidden='true' />
+                    <IoClose
+                      className='
+                      h-6
+                      w-6
+                    '
+                    />
                   </button>
                 </div>
                 {children}
