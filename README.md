d7b4fb9c3797717ad41748bf9460db2b93ebfca5
wisdompandamaster <2532569059@qq.com>
2023-07-27 13:52:32 +0800

    feat(zustand):create a store use zustand

--- README.md
+++ README.md
@@ -56,3 +56,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 - axios
 - headlessui/react
 - react-spinners
+- zustand (轻量状态管理工具)
---
+++ app/hooks/useActiveList.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+// 一个轻量状态管理工具
+
+interface ActiveListStore {
+  members: string[];
+  add: (id: string) => void;
+  remove: (id: string) => void;
+  set: (id: string[]) => void;
+}
+
+// 创建一个全局唯一 store
+const useActiveList = create<ActiveListStore>(set => ({
+  members: [],
+  //  添加
+  add: id => set(state => ({ members: [...state.members, id] })),
+  //   删除
+  remove: id =>
+    set(state => ({
+      members: state.members.filter(memberId => memberId !== id),
+    })),
+  // set
+  set: ids => set({ members: ids }),
+}));
+
+export default useActiveList;
