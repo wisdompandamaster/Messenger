9687c6507244499876333a80b00bbbe482176121
wisdompandamaster <2532569059@qq.com>
2023-08-21 00:21:19 +0800

    feat(RequestCard):initial the style of card

--- app/users/components/RequestCard.tsx
+++ app/users/components/RequestCard.tsx
@@ -12,14 +12,22 @@ const RequestCard: React.FC<RequestCardProps> = ({ user }) => {
   return (
     <div
       className='
-        flex
+        flex 
+        gap-4 
+        items-center
+        h-20
+        w-72
+        p-5
+        bg-neutral-100
+        rounded-xl
+        m-5
     '
     >
       <Avatar user={user} />
-      <div className='w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md'>
+      <div className='w-8 h-8 bg-sky-400 hover:bg-sky-500 grid place-items-center rounded-full transition hover:shadow-md'>
         <HiCheck className='font-semibold text-white w-3/4 h-3/4' />
       </div>
-      <div className='w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md'>
+      <div className='w-8 h-8 bg-rose-500 hover:bg-rose-600 grid place-items-center rounded-full transition hover:shadow-md'>
         <HiX className='font-semibold text-white w-3/4 h-3/4' />
       </div>
     </div>
